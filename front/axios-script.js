5faacea92cd06b2e6b7fb0b9a83710abe1567c08
jeppi94 <jeppi94@naver.com>
2025-01-23 13:15:59 +0900

    제목 및 axios url 변경

--- front/axios-script.js
+++ front/axios-script.js
@@ -30,7 +30,7 @@ async function getDataSet(category) {
 
   const dataSet = await axios({
     method: "get",
-    url: `http://localhost:3000/restaurants?category=${qs}`,
+    url: `http://3.34.15.83:3000/restaurants?category=${qs}`,
     headers: {},
     data: {},
   });
